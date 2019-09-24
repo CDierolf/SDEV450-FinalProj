ab8d16e45f0db0b5f9ef96b9b7abfd9f487da5ce
Christopher Dierolf <chidi117@gmail.com>
2019-09-24 08:49:23 -0500

    initial commit

---
+++ src/sdev_450_ticketmanager/SDEV_450_TicketManager.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sdev_450_ticketmanager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/** 
+ * @Course: SDEV 450 ~ Java Programming III
+ * @Author Name: Christopher K. Dierolf, Thomas Muck, Neil Hart, Stephen Graybeal
+ * @Assignment Name: SDEV-450 Final Project - Ticket Master
+ * @Date: Sep 24, 2019
+ * @Description: Entry point for the application. Loads the LoginView
+ */
+public class SDEV_450_TicketManager extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/LoginView/LoginView.fxml"));
+        
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
