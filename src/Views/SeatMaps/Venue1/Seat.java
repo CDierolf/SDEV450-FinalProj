92cb9495750e8b91734ad2ea79784d3ee52c6f01
Neil Hart <neilchart@gmail.com>
2019-10-07 01:29:18 -0400

    Implementing Seat class

--- src/Views/SeatMaps/Venue1/Seat.java
+++ src/Views/SeatMaps/Venue1/Seat.java
@@ -1,33 +1,71 @@
 package Views.SeatMaps.Venue1;
 
-import javafx.scene.layout.StackPane;
+//Imports
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextBoundsType;
 
 /** 
  * @Course: SDEV 350 ~ Java Programming II
- * @Author Name: Christopher K. Dierolf
- * @Assignment Name: Views.SeatMaps.Venue1
+ * @Author Name: Neil Hart
+ * @Assignment Name: SDEV 450 - Final Project
  * @Date: Oct 6, 2019
- * @Subclass Seat Description: 
+ * @Subclass Seat Description: Represents the seats in venue view
  */
-//Imports
 
 //Begin Subclass Seat
-public class Seat extends Circle{
+public class Seat {
     
-    private int seatNumber;
-    private int rowNumber;
+    // Instance variables
+    private final int seatNumber; // Final ivar - set in constructor
+    private final int rowNumber; // Final ivar - set in constructor
+    private Circle seatImage;
     private boolean isAvailable;
     
-    public Circle createSeat() {
-        Circle seat = new Circle();
-        seat.setFill(Color.DODGERBLUE);
-        return seat;
+    // Getter methods
+    // No setters should be required as set in constructor
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public boolean isIsAvailable() {
+        return isAvailable;
+    }
+    
+    // Constructor
+    public Seat(int seatNumber, int rowNumber, boolean isAvailable) {
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
+        this.isAvailable = isAvailable;
         
-        
+        // Create circle on initialization
+        this.createSeatImage();
+    }
+    
+    // Create circle representation of seat for Venue view
+    private void createSeatImage() {
+        Circle seatImg = new Circle();
+        this.seatImage = seatImg;
+        // Color seat image based on availability
+        updateImage();
+    }
+    
+    // Update circle image color based on availability
+    private void updateImage() {
+        if (isAvailable) {
+            seatImage.setFill(Color.DODGERBLUE);
+        } else {
+            seatImage.setFill(Color.RED);
+        }
+    }
+    
+    // Sell seat - change availability and display
+    public void sellSeat() {
+        isAvailable = false;
+        updateImage();
     }
 
 } //End Subclass Seat
