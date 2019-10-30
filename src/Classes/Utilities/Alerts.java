f76f1a306cc89f3a0a68025ec478afda7fa4c72f
Christopher Dierolf <chidi117@gmail.com>
2019-10-30 17:45:33 -0500

    fill out validation and alerts classes

--- src/Classes/Utilities/Alerts.java
+++ src/Classes/Utilities/Alerts.java
@@ -13,6 +13,66 @@ import javafx.scene.control.Alert;
 
 //Begin Subclass Alerts
 public class Alerts {
-
+    Alert alert = new Alert(Alert.AlertType.WARNING);
     
+    /**
+     * Provides an alert for a blank field.
+     * @return 
+     */
+    public Alert blankFieldAlert(String fieldID) {
+        alert.setTitle("Blank Field Found");
+        alert.setHeaderText(fieldID + " cannot be blank.");
+        alert.setContentText("Please provide a valid value for "
+                + fieldID + ".");
+        
+        return alert;
+    }
+    
+    /**
+     * Provides an alert for invalid characters within a field.
+     * @return 
+     */
+    public Alert invalidIntegerAlert(String fieldID) {
+        alert.setTitle("Invalid Value Entered");
+        alert.setHeaderText(fieldID + " must have at least one number\n"
+                + "and contain numbers only.");
+        alert.setContentText("Please provide a valid value for " + fieldID);
+        
+        return alert;
+    }
+    
+    public Alert invalidDoubleAlert(String fieldID) {
+        alert.setTitle("Invalid Decimal Value Entered");
+        alert.setHeaderText(fieldID + " must contain a decimal value only without 0's");
+        alert.setContentText("Please provide a valid decimal value for " + fieldID);
+        
+        return alert;
+    }
+    
+    public Alert invalidNameAlert(String fieldID) {
+        alert.setTitle("Invalid Name");
+        alert.setHeaderText(fieldID + " must contain at least one character,"
+                + "\nand cannot contain numbers or special characters.");
+        alert.setContentText("Please provide a valid value for " + fieldID);
+        
+        return alert;
+    }
+    
+    public Alert stackEmptyAlert() {
+        alert.setTitle("Nothing to Sort");
+        alert.setHeaderText("There is no longer any values to remove from the"
+                + " list.");
+        alert.setContentText("You must add values to the list first.");
+        
+        return alert;
+    }
+    
+    public Alert genericAlert(String title, String header, String content) {
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        
+        return alert;
+    }
+
 } //End Subclass Alerts
--- src/Classes/Utilities/Validation.java
+++ src/Classes/Utilities/Validation.java
@@ -1,5 +1,8 @@
 package Classes.Utilities;
 
+import java.util.List;
+import javafx.scene.control.CheckBox;
+
 /** 
  * @Course: SDEV 350 ~ Java Programming II
  * @Author Name: Christopher K. Dierolf
@@ -10,6 +13,191 @@ package Classes.Utilities;
 //Imports
 
 //Begin Subclass Validation
-public class Validation {
+public class Validation extends Alerts{
+    
+    private final String numericalRegex = "^[0-9]*$";
+    private final String nameRegex = "^[A-Za-z\\s]*$";
+    private final String doubleRegex = "-?\\d+(\\.\\d+)?";
+    private final String emailRegex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)"
+                        + "*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-} //End Subclass Validation
+    /**
+     * Validates string against regex and displays alert if needed
+     *
+     * @param s
+     * @param displayAlert
+     * @return
+     */
+    public boolean validateIntegerInput(String s, String fieldID, boolean displayAlert) {
+
+        String s1 = s.trim();
+        boolean validRegex = matchesRegex(s, numericalRegex);
+        boolean isValid = false;
+
+        if (!validRegex || s1.isEmpty()) {
+            isValid = false;
+        } else {
+            isValid = true;
+        }
+        
+        if (displayAlert) {
+            invalidIntegerAlert(fieldID).showAndWait();
+        }
+        
+        return isValid;
+    }
+    
+    /** OVERLOADED
+     * Validates a string aginst a regex. Does not contain fieldID or display
+     * Alert parameters.
+     * @param s
+     * @return 
+     */
+    public boolean validateIntegerInput(String s) {
+        String s1 = s.trim();
+        boolean validRegex = matchesRegex(s, numericalRegex);
+        boolean isValid = false;
+        
+        if (!validRegex || s1.isEmpty()) {
+            isValid = false;
+        } else {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    /**
+     * Validates if string contains data
+     *
+     * @param s
+     * @return
+     */
+    public boolean validateForBlankInput(String s, String fieldID,
+            boolean displayAlert) {
+
+        boolean isInvalid = s.isEmpty() || s.trim().length() == 0;
+        boolean inputValid = false;
+
+        if (displayAlert) {
+            if (isInvalid) {
+                blankFieldAlert(fieldID).showAndWait();
+                inputValid = false;
+            } else {
+                inputValid = true;
+            }
+        } else {
+            if (isInvalid) {
+                // Don't display alert
+                inputValid = false;
+            } else {
+                inputValid = true;
+            }
+        }
+
+        return inputValid;
+    }
+
+    /**
+     * Validates a double value by parseDouble(s).
+     *
+     * @param s - string to be validated
+     * @param fieldID - ID of the field containing String s
+     * @param displayAlert - display alert (true/false)
+     * @return
+     */
+    public boolean validateDoubleInput(String s, String fieldID, boolean displayAlert) {
+        String s1 = s.trim();
+        boolean isValid = false;
+        boolean hasZero = String.valueOf(s1).contains("0");
+        boolean validRegex = matchesRegex(s1, doubleRegex);
+
+        if (!s1.isEmpty() && validRegex) {
+            if (!hasZero) {
+                isValid = true;
+            } else {
+                isValid = false;
+            }
+        } else if (s1.isEmpty()) {
+            isValid = true;
+        }
+
+        if (displayAlert && !isValid) {
+            invalidDoubleAlert(fieldID).showAndWait();
+        }
+
+        return isValid;
+    }
+
+    public boolean validateNameInput(String s, String fieldID, boolean displayAlert) {
+        String s1 = s.trim();
+        boolean isValid = false;
+        boolean validRegex = matchesRegex(s1, nameRegex);
+
+        if (!validRegex || s1.isEmpty()) {
+            isValid = false;
+        } else {
+            isValid = true;
+        }
+
+        if (displayAlert && !isValid) {
+            invalidNameAlert(fieldID).showAndWait();
+        }
+
+        return isValid;
+    }
+
+    /**
+     * Validates if only one checkbox in lstChecks is selected.
+     *
+     * @param lstChecks
+     * @return true only one selected, false more than one selected
+     */
+    public boolean oneCheckSelected(List<CheckBox> lstChecks, boolean displayAlert) {
+        int numChecks = 0;
+        boolean valid = false;
+        for (CheckBox c : lstChecks) {
+            if (c.isSelected()) {
+                numChecks++;
+            }
+        }
+        if (!(numChecks > 1)) {
+            valid = true;
+        } else {
+            valid = false;
+        }
+
+        if (!valid && displayAlert) {
+            genericAlert("Select One Checkbox", "One one checkbox may be selected",
+                    "Please ensure that only one checkbox is selected.").showAndWait();
+        }
+        return valid;
+    }
+    
+    public boolean validateFilePath(String s, String fieldID, boolean displayAlert) {
+        String s1 = s.trim();
+        boolean isValid = false;
+        if (s1.isEmpty()) {
+            isValid = false;
+        } else {
+            isValid = true;
+        }
+        if (!isValid && displayAlert) {
+            genericAlert("File not loaded", "Unable to obtain data.", 
+                    "Please load a file first.").showAndWait();
+        }
+        
+        return isValid;
+    }
+
+    /**
+     * Returns result of match between input and the provided regex expression
+     *
+     * @param input - String input
+     * @param regex - String regex
+     * @return
+     */
+    private boolean matchesRegex(String input, String regex) {
+        return input.matches(regex);
+    }
+
+} //End Subclass ValidationUtils
