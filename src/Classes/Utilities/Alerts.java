a130a786d1dc7a8b9cc6a8eb00c135bbdee592f5
Christopher Dierolf <chidi117@gmail.com>
2019-10-30 17:42:29 -0500

    Begin work on alert and validation classes

---
+++ src/Classes/Utilities/Alerts.java
@@ -0,0 +1,18 @@
+package Classes.Utilities;
+
+import javafx.scene.control.Alert;
+
+/** 
+ * @Course: SDEV 350 ~ Java Programming II
+ * @Author Name: Christopher K. Dierolf
+ * @Assignment Name: Classes.Utilities
+ * @Date: Oct 30, 2019
+ * @Subclass Alerts Description: 
+ */
+//Imports
+
+//Begin Subclass Alerts
+public class Alerts {
+
+    
+} //End Subclass Alerts
---
+++ src/Classes/Utilities/Validation.java
@@ -0,0 +1,15 @@
+package Classes.Utilities;
+
+/** 
+ * @Course: SDEV 350 ~ Java Programming II
+ * @Author Name: Christopher K. Dierolf
+ * @Assignment Name: Classes.Utilities
+ * @Date: Oct 30, 2019
+ * @Subclass Validation Description: 
+ */
+//Imports
+
+//Begin Subclass Validation
+public class Validation {
+
+} //End Subclass Validation
