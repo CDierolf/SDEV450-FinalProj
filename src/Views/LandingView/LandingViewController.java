5eadc98a49b921a5feb8a4501c813e1da7ac3496
jackofallcodes <47017711+jackofallcodes@users.noreply.github.com>
2019-12-04 13:03:20 -0500

    Styled landing button for no events

--- src/Views/LandingView/LandingViewController.java
+++ src/Views/LandingView/LandingViewController.java
@@ -213,7 +213,18 @@ public class LandingViewController implements Initializable {
     private void displayErrorTop() {
         Label l = new Label("You haven't purchased any events yet");
         Button b = new Button("Find some events");
-        b.setStyle("shiny-orange");
+        b.setStyle("-fx-background-color: \n"
+                + "        linear-gradient(#ffd65b, #e68400),\n"
+                + "        linear-gradient(#ffef84, #f2ba44),\n"
+                + "        linear-gradient(#ffea6a, #efaa22),\n"
+                + "        linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%),\n"
+                + "        linear-gradient(from 0% 0% to 15% 50%, rgba(255,255,255,0.9), rgba(255,255,255,0));\n"
+                + "    -fx-background-radius: 30;\n"
+                + "    -fx-background-insets: 0,1,2,3,0;\n"
+                + "    -fx-text-fill: #654b00;\n"
+                + "    -fx-font-weight: bold;\n"
+                + "    -fx-font-size: 14px;\n"
+                + "    -fx-padding: 10 20 10 20;");
         b.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
@@ -232,7 +243,18 @@ public class LandingViewController implements Initializable {
     private void displayErrorBot() {
         Label l = new Label("There are no upcoming events nearby");
         Button b = new Button("Find some events");
-        b.setStyle("shiny-orange");
+        b.setStyle("-fx-background-color: \n"
+                + "        linear-gradient(#ffd65b, #e68400),\n"
+                + "        linear-gradient(#ffef84, #f2ba44),\n"
+                + "        linear-gradient(#ffea6a, #efaa22),\n"
+                + "        linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%),\n"
+                + "        linear-gradient(from 0% 0% to 15% 50%, rgba(255,255,255,0.9), rgba(255,255,255,0));\n"
+                + "    -fx-background-radius: 30;\n"
+                + "    -fx-background-insets: 0,1,2,3,0;\n"
+                + "    -fx-text-fill: #654b00;\n"
+                + "    -fx-font-weight: bold;\n"
+                + "    -fx-font-size: 14px;\n"
+                + "    -fx-padding: 10 20 10 20;");
         b.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
